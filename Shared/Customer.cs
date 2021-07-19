04eca4be06fbf7599af9d6e414e7c0729e963929
Hossam Fathy <semsem.hos33@hotmail.com>
2021-07-19 07:22:25 +0200

    Fix Names on Json file to meet data.json

--- Shared/Customer.cs
+++ Shared/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -8,13 +9,18 @@ namespace Customers.Shared
     public class Customer
     {
         [Required]
+        [JsonProperty(PropertyName = "firstName")]
         public string FristName { get; set; }
         [Required]
+        [JsonProperty(PropertyName = "last_name")]
         public string LastName { get; set; }
         [Required]
         [EmailAddress]
+        [JsonProperty(PropertyName = "emailAddress")]
         public string EmailAddress { get; set; }
+        [JsonProperty(PropertyName = "subscribed")]
         public bool Subscribed { get; set; }
+        [JsonProperty(PropertyName = "dob")]
         public DateTime Dob { get; set; }
     }
 }
